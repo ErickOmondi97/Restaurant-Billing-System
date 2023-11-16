7f67136501305c079a92c9664e52987b4446cc02
Erick Omondi <eriqueasaria@gmail.com>
2023-11-16 23:24:35 +0300

    Add files via upload

---
+++ src/RestaurantBillingSystem/Bill.java
@@ -0,0 +1,14 @@
+package RestaurantBillingSystem;
+import java.util.ArrayList;
+
+public class Bill {
+  private ArrayList<String> orderedFood;
+  private ArrayList<Integer> orderedQuantity;
+
+  public Bill(){
+    orderedFood = new ArrayList<String>();
+    orderedQuantity = new ArrayList<Integer>();
+  }
+
+  private double 
+}
---
+++ src/RestaurantBillingSystem/Main.java
@@ -0,0 +1,7 @@
+package RestaurantBillingSystem;
+
+public class Main {
+  public static void main(String[] args){
+    
+  }
+}
